bbd36689349d105b65941f541fd81fa928f9c135
Florquin Chelsea <florquinchelsea@gmail.com>
2024-12-17 09:49:14 +0000

    Add new file

---
+++ Java/Exercice.java
@@ -0,0 +1,40 @@
+Chelsea Florquin BTS 1 Décembre 2024
+
+package FonctionsProc3;
+
+public class localité {
+
+	public static void main(String[] args) {
+		int val = 10;
+		augmenter(val);
+		System.out.println("valeur de val en sortie :"+val);
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void augmenter(int val) {
+		{val=val+5;
+		System.out.println("valeur de val dans la procédure"+val);
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
+package JAVASIO;
+import java.util.Scanner;
+public class premiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner masaisie =new Scanner(System.in);
+double x=0;
+while (x<=0 || x>5) {
+	System.out.println("entrer un reel");
+	x=masaisie.nextDouble();
+}
+	}
+
+}
