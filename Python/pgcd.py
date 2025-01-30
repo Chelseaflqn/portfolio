c60354b97c8ccedf06c1ea615338af60461ec838
Florquin Chelsea <florquinchelsea@gmail.com>
2025-01-30 13:21:00 +0000

    Add new file

---
+++ Python/pgcd.py
@@ -0,0 +1,12 @@
+//Chelsea Florquin BTS1 30/01/2025
+def pgcd(a,b):
+    if b==0
+    return a
+
+    else:
+        r=a%b
+        while r!=0:
+            a=b
+            b=r
+            r=a%b
+        return(b)
